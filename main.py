d3b52588314d11097926b2556aa77ad103e8ab94
Rinat Khafizov <freerinch@gmail.com>
2023-04-04 23:53:39 +0800

    update main

--- main.py
+++ main.py
@@ -37,42 +37,103 @@
 #         print('Фотография успешно загружена на Яндекс.Диск')
 #     else:
 #         print('Ошибка загрузки фотографии на Яндекс.Диск')
-import requests
+# import requests
+# import time
+# from datetime import datetime
+# class VK:
+#     def _init_(self, token):
+#         self.token = token
+#
+#     def get_photos(self):
+#         URL = 'https://api.vk.com/method/photos.get'
+#         params = {'access_token': self.token,
+#               'v':'5.131',
+#               'album_id': 'profile',
+#               'photo_sies': '1',
+#               'extended': '1'
+#               }
+#
+#         res = requests.get(URL, params=params)
+#         res = res.json()
+#         dict_with_photos = {}
+#
+#         for i in res['response']['items']:
+#             date_of_photo = i['date']
+#             date_time = datetime.fromtimestamp(date_of_photo).strftime('%Y-%m-%d')
+#             name_of_photo = str(i['likes']['count']) + date_time
+#
+#             index_photo = len(i['sizes'])
+#             url_photo = i['sizes'][index_photo - 1]['url']
+#             type_photo = i['sizes'][index_photo - 1]['type']
+#             dict_with_photos[name_of_photo] = url_photo, type_photo
+#
+#         return dict_with_photos
+#
+# if __name__ == "__main__":
+#     with open('TOKEN.txt', 'r') as file_object:
+#         token = file_object.read().strip()
+#     print(token)
+# Vk_photos = VK(token)
+# dict_with_photos = Vk_photos.get_photos()
+# print(dict_with_photos)
 import time
 from datetime import datetime
-class VK:
-    def _init_(self, token):
+import requests
+from pprint import pprint
+
+class Vk:
+    URL = 'https://api.vk.com/method/photos.get'
+    def __init__(self,token):
         self.token = token
+        self.params = {'access_token': self.token,
+                  'v': '5.131',
+                  'album_id': 'profile',
+                  'photo_sies': '1',
+                  'extended': '1'
+                  }
+    def show_token(self):
+        print(self.token)
 
     def get_photos(self):
-        URL = 'https://api.vk.com/method/photos.get'
-        params = {'access_token': self.token,
-              'v':'5.131',
-              'album_id': 'profile',
-              'photo_sies': '1',
-              'extended': '1'
-              }
-
-        res = requests.get(URL, params=params)
+        res = requests.get(Vk.URL, params=self.params)
         res = res.json()
         dict_with_photos = {}
-
         for i in res['response']['items']:
-            date_of_photo = i['date']
-            date_time = datetime.fromtimestamp(date_of_photo).strftime('%Y-%m-%d')
-            name_of_photo = str(i['likes']['count']) + date_time
+                date_of_photo = i['date']
+                date_time = datetime.fromtimestamp(date_of_photo).strftime('%Y-%m-%d')
+                name_of_photo = str(i['likes']['count']) + date_time
 
-            index_photo = len(i['sizes'])
-            url_photo = i['sizes'][index_photo - 1]['url']
-            type_photo = i['sizes'][index_photo - 1]['type']
-            dict_with_photos[name_of_photo] = url_photo, type_photo
+                index_photo = len(i['sizes'])
+                url_photo = i['sizes'][index_photo - 1]['url']
+                type_photo = i['sizes'][index_photo - 1]['type']
+                dict_with_photos[name_of_photo] = url_photo, type_photo
+        print(f'В словарь помещено {len(dict_with_photos.keys())} фотографий')
 
         return dict_with_photos
+
+class YaDisk:
+    URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    pass
+    # def __init__(self,token):
+
+
+    #     url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    #     headers = {'Authorization': f'OAuth {token_YA}'}
+    #     params = {'path': f'/vk_photo/{name_of_photo}', 'overwrite': 'true'}
+    #     response = requests.get(url, headers=headers, params=params)
+    #     upload_url = response.json()['href']
+    #     response = requests.put(upload_url, headers=headers, data=requests.get(url_photo).content)
+    #     if response.status_code == 201:
+    #         print('Фотография успешно загружена на Яндекс.Диск')
+    #     else:
+    #         print('Ошибка загрузки фотографии на Яндекс.Диск')
+
+
 
 if __name__ == "__main__":
     with open('TOKEN.txt', 'r') as file_object:
         token = file_object.read().strip()
-    print(token)
-Vk_photos = VK(token)
-dict_with_photos = Vk_photos.get_photos()
-print(dict_with_photos)
+
+    Vk = Vk(token)
+    Vk.show_token()
+    Vk.get_photos()
